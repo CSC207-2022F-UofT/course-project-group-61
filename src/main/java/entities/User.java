56de79515b6d193a96ee675862ad040daecf94d7
Jacob DeSousa <jacobd2009@hotmail.com>
2022-10-31 15:24:43 -0400

    Test GitHub Permissions 3

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -8,6 +8,7 @@ public abstract class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        System.out.println("Test Again");
     }
 
     public String getUsername() {
