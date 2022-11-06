cbefc76e22d458c0f9dba0ff2aa54eefbd9387ca
andriiostrolutskyi <andrii.ostrolutskyi@ukma.edu.ua>
2022-11-06 18:43:59 -0500

    create Product entity

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,4 +1,38 @@
 package entities;
 
 public class Product {
+
+    private String name;
+    private long UPC;
+    private int price;
+
+    public Product(String name, int UPC, int price){
+        this.name = name;
+        this.UPC = UPC;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public long getUPC(){
+        return UPC;
+    }
+
+    public void setUPC(int newUPC){
+        this.UPC = newUPC;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int newPrice){
+        this.price = newPrice;
+    }
 }
