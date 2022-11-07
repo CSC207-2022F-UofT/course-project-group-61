8e209ba21f1c6a3e7fb35bcf8e51ff47bb67561c
Max Elinson <max.elinson@gmail.com>
2022-11-07 11:55:01 -0500

    adjust Facility attribute types and tests

--- src/main/java/entities/Facility.java
+++ src/main/java/entities/Facility.java
@@ -6,10 +6,17 @@ import java.util.UUID;
 public class Facility {
     private final String name;
     private final UUID facilityID;
-    private HashMap<Integer, Integer> inventory;
+    private HashMap<Long, Integer> inventory;
     private String facilityType;
 
-    public Facility(String name, HashMap<Integer, Integer> inventory, String facType) {
+    public Facility(String name, String facType) {
+        this.name = name;
+        this.facilityID = UUID.randomUUID();
+        this.inventory = new HashMap<Long, Integer>();
+        this.facilityType = facType;
+    }
+
+    public Facility(String name, HashMap<Long, Integer> inventory, String facType) {
         this.name = name;
         this.facilityID = UUID.randomUUID();
         this.inventory = inventory;
@@ -28,15 +35,15 @@ public class Facility {
         return this.facilityType;
     }
 
-    public int getUPCQuantity(int upc) {
+    public int getUPCQuantity(Long upc) {
         return this.inventory.get(upc);
     }
 
-    public void addProduct(int upc, int quantity) {
+    public void addProduct(Long upc, int quantity) {
         this.inventory.put(upc, this.inventory.get(upc) + quantity);
     }
 
-    public void removeProduct(int upc, int quantity) {
+    public void removeProduct(Long upc, int quantity) {
         this.inventory.put(upc, this.inventory.get(upc) - quantity);
     }
 }
--- src/main/java/newfacility/NewFacilityInteractor.java
+++ src/main/java/newfacility/NewFacilityInteractor.java
@@ -1,4 +1,20 @@
 package newfacility;
 
+import database.FacilityDbGateway;
+import entities.Facility;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class NewFacilityInteractor {
+
+    FacilityDbGateway readWriter = new FacilityDbGateway();
+    private HashMap<UUID, Facility> facilities = readWriter.getAllFacilities();
+    private Facility newFacility;
+
+    public NewFacilityInteractor(String name, String facType) {
+        this.newFacility = new Facility(name, facType);
+    }
+
+
 }
