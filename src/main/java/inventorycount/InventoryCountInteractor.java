940af8ddad5376a348ea6f62395f3e9ee95f7ed8
Ron Ulitsky <ron.ulitsky@gmail.com>
2022-11-07 01:55:58 -0500

    implemented interactor logic for inventory counts.

--- src/main/java/inventorycount/InventoryCountInteractor.java
+++ src/main/java/inventorycount/InventoryCountInteractor.java
@@ -1,4 +1,53 @@
 package inventorycount;
 
+
+import database.FacilityDb;
+import database.FacilityDbGateway;
+import entities.Facility;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.UUID;
+
 public class InventoryCountInteractor {
+
+    private final FacilityDb facilityDb;
+    private final UUID facID;
+
+    public InventoryCountInteractor(UUID facID){
+
+        this.facilityDb = new FacilityDbGateway();
+
+        this.facID = facID;
+
+    }
+
+    public void updateInventoryCount(HashMap<Integer, Integer> newInventoryCount){
+        // pull facility entity
+        Facility facility = facilityDb.getFacility(facID);
+
+        // update inventory
+        for (int upc : newInventoryCount.keySet()){
+
+            int newCount = newInventoryCount.get(upc);
+
+            int currentCount = facility.getUPCQuantity(upc);
+
+            int countDelta = newCount - currentCount;
+
+            facility.addProduct(upc, countDelta);
+
+        }
+
+        // save entity
+        facilityDb.updateFacility(facility);
+
+    }
+
+    public HashMap<Integer, Integer> getCurrentCount(){
+        Facility facility = facilityDb.getFacility(facID);
+
+        return facility.getInventory();
+    }
+
 }
