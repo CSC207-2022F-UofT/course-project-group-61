193194293201ed42ff1bfa54faf55bdde826fcd6
Max Elinson <max.elinson@gmail.com>
2022-11-10 13:18:00 -0500

    complete ItemLookupInteractor/Controller with assumptions that only valid inputs will be inputted complete tests for these methods

--- src/main/java/itemlookup/ItemLookupInteractor.java
+++ src/main/java/itemlookup/ItemLookupInteractor.java
@@ -1,4 +1,34 @@
 package itemlookup;
 
+import database.ProductDbGateway;
+import entities.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class ItemLookupInteractor {
+
+    ProductDbGateway readWriter = new ProductDbGateway();
+    public ItemLookupInteractor() {
+
+    }
+
+    //TODO: what to return if upc doesn't exist
+    public Product lookupByUPC(Long upc) {
+        HashMap<Long, Product> products = this.readWriter.getAllProducts();
+        return products.get(upc);
+    }
+
+    //TODO: what to return if name doesn't exist
+    public Product lookupByName(String name) {
+        HashMap<Long, Product> products = this.readWriter.getAllProducts();
+
+        for(Map.Entry<Long, Product> entry : products.entrySet()) {
+            if (Objects.equals(name, entry.getValue().getName())) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
 }
---
+++ src/test/java/itemlookup/ItemLookupTest.java
@@ -0,0 +1,51 @@
+package itemlookup;
+
+/* things to test:
+    CURRENTLY ONLY IMPLEMENTED FOR NON-EMPTY DATABASE WITH VALID LOOKUP INPUTS
+    - lookup by upc
+    -lookup by name
+     */
+
+import database.ProductDbGateway;
+import entities.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ItemLookupTest {
+
+    private ItemLookupController itemLookupController;
+    private ProductDbGateway productDbGateway;
+    private Product testProduct;
+
+    public boolean checkAttributes(Product product, String expName, Long expUPC, int expPrice) {
+        return Objects.equals(product.getName(), expName) & Objects.equals(product.getUPC(), expUPC) &
+                Objects.equals(product.getPrice(), expPrice);
+    }
+
+
+
+    @Before
+    public void setup() {
+        this.itemLookupController = new ItemLookupController();
+        this.productDbGateway = new ProductDbGateway();
+        this.productDbGateway.fileReset();
+        this.testProduct = new Product("Apple", 123456789123L, 5);
+        this.productDbGateway.updateProduct(this.testProduct);
+    }
+
+    @Test
+    public void testLookupByUPC() {
+        assertTrue(checkAttributes(this.itemLookupController.lookupByUPC(123456789123L), "Apple", 123456789123L, 5));
+    }
+
+    @Test
+    public void testLookupByName() {
+        assertTrue(checkAttributes(this.itemLookupController.lookupByName("Apple"), "Apple", 123456789123L, 5));
+    }
+
+}
