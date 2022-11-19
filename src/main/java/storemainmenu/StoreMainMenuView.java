2b17998eb43f20471083b726b2bbf3e5d16042dc
Jacob DeSousa <jacobd2009@hotmail.com>
2022-11-19 17:40:38 -0500

    Integrate StoreMainMenuViewModel with UserLoginPresenter Added sample logic in Main

--- src/main/java/storemainmenu/StoreMainMenuView.java
+++ src/main/java/storemainmenu/StoreMainMenuView.java
@@ -65,7 +65,6 @@ public class StoreMainMenuView extends JFrame implements Observer, ActionListene
         setTitle("Inventory Management System");
         setLayout(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
     }
 
     @Override
