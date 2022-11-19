275d639e611295e36d89e27ebb5540d35433799b
Max Elinson <max.elinson@gmail.com>
2022-11-19 17:24:52 -0500

    small update for testibility

--- src/main/java/storemainmenu/StoreMainMenuPresenter.java
+++ src/main/java/storemainmenu/StoreMainMenuPresenter.java
@@ -12,18 +12,27 @@ public class StoreMainMenuPresenter {
         viewModel.setVisible(false);
         switch (option) {
             case PLACE_ORDER:
+                System.out.println("place order");
                 //TODO: set OrderView to visible
                 break;
             case DAILY_SALES:
+                System.out.println("daily sales");
+
                 //TODO: set OrderView to visible
                 break;
             case INV_COUNT:
+                System.out.println("inv count");
+
                 //TODO: set OrderView to visible
                 break;
             case GEN_REPORT:
+                System.out.println("gen report");
+
                 //TODO: set OrderView to visible
                 break;
             case ITEM_LOOKUP:
+                System.out.println("item lookup");
+
                 //TODO: set OrderView to visible
                 break;
         }
