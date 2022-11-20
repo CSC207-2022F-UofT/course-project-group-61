b3144429f479631745fe1efaf2115d14f8bdadd7
jacobdesousa <jacobd2009@hotmail.com>
2022-11-20 14:17:05 -0500

    Add main menu initialization to main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import adminmainmenu.AdminMainMenuController;
+import adminmainmenu.AdminMainMenuPresenter;
+import adminmainmenu.AdminMainMenuView;
 import adminmainmenu.AdminMainMenuViewModel;
 import database.UserDbGateway;
 import entities.FacilityType;
 import entities.FacilityUser;
+import storemainmenu.StoreMainMenuController;
+import storemainmenu.StoreMainMenuPresenter;
+import storemainmenu.StoreMainMenuView;
 import storemainmenu.StoreMainMenuViewModel;
 import userlogin.*;
+import warehousemainmenu.WarehouseMainMenuController;
+import warehousemainmenu.WarehouseMainMenuPresenter;
+import warehousemainmenu.WarehouseMainMenuView;
 import warehousemainmenu.WarehouseMainMenuViewModel;
 
 public class Main {
@@ -17,6 +26,15 @@ public class Main {
         UserLoginView loginView = new UserLoginView(new UserLoginController(new UserLoginInteractor(new UserLoginPresenter(loginViewModel, storeViewModel, warehouseViewModel, adminViewModel), new UserDbGateway())));
         loginViewModel.addObserver(loginView);
         loginViewModel.setVisible(true);
+
+        StoreMainMenuView storeMainMenuView = new StoreMainMenuView(new StoreMainMenuController(new StoreMainMenuPresenter(storeViewModel)));
+        storeViewModel.addObserver(storeMainMenuView);
+
+        WarehouseMainMenuView warehouseMainMenuView = new WarehouseMainMenuView(new WarehouseMainMenuController(new WarehouseMainMenuPresenter(warehouseViewModel)));
+        warehouseViewModel.addObserver(warehouseMainMenuView);
+
+        AdminMainMenuView adminMainMenuView = new AdminMainMenuView(new AdminMainMenuController(new AdminMainMenuPresenter(adminViewModel)));
+        adminViewModel.addObserver(adminMainMenuView);
 
         UserDbGateway gateway = new UserDbGateway();
         gateway.fileReset();
