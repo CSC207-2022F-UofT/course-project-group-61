30813eac8afbd5268a32290c90a4995e0ff810d5
tadanokaida <kevinkhuang2002@gmail.com>
2022-11-21 15:58:08 -0500

    Updated main to include newUser use case. Removed automatic reset of userDB file (user.ser) in order to be able to test how newUser saves new users in database.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import database.OrderDbGateway;
 import database.ProductDbGateway;
 import database.UserDbGateway;
 import entities.*;
+import newuser.NewUserViewModel;
 import order.*;
 import storemainmenu.StoreMainMenuController;
 import storemainmenu.StoreMainMenuPresenter;
@@ -17,6 +18,7 @@ import warehousemainmenu.WarehouseMainMenuController;
 import warehousemainmenu.WarehouseMainMenuPresenter;
 import warehousemainmenu.WarehouseMainMenuView;
 import warehousemainmenu.WarehouseMainMenuViewModel;
+import newuser.*;
 
 import java.util.UUID;
 
@@ -30,6 +32,7 @@ public class Main {
         WarehouseMainMenuViewModel warehouseViewModel = new WarehouseMainMenuViewModel();
         AdminMainMenuViewModel adminViewModel = new AdminMainMenuViewModel();
         OrderViewModel orderViewModel = new OrderViewModel();
+        NewUserViewModel newUserViewModel = new NewUserViewModel();
 
         UserLoginView loginView = new UserLoginView(new UserLoginController(new UserLoginInteractor(new UserLoginPresenter(loginViewModel, storeViewModel, warehouseViewModel, adminViewModel), new UserDbGateway())));
         loginViewModel.addObserver(loginView);
@@ -41,24 +44,27 @@ public class Main {
         WarehouseMainMenuView warehouseMainMenuView = new WarehouseMainMenuView(new WarehouseMainMenuController(new WarehouseMainMenuPresenter(warehouseViewModel)));
         warehouseViewModel.addObserver(warehouseMainMenuView);
 
-        AdminMainMenuView adminMainMenuView = new AdminMainMenuView(new AdminMainMenuController(new AdminMainMenuPresenter(adminViewModel)));
+        AdminMainMenuView adminMainMenuView = new AdminMainMenuView(new AdminMainMenuController(new AdminMainMenuPresenter(adminViewModel, newUserViewModel)));
         adminViewModel.addObserver(adminMainMenuView);
 
         OrderView orderView = new OrderView(new OrderController(new OrderInteractor(new OrderPresenter(orderViewModel, storeViewModel), new OrderDbGateway(), new FacilityDbGateway(), new ProductDbGateway())), orderViewModel);
         orderViewModel.addObserver(orderView);
 
-        UserDbGateway userDb = new UserDbGateway();
-        userDb.fileReset();
-        userDb.updateUser(new FacilityUser("Jacob", "Test", UUID.randomUUID(), FacilityType.STORE));
+        NewUserView newUserView = new NewUserView(new NewUserController(new NewUserInteractor(new NewUserPresenter(newUserViewModel, adminViewModel), new UserDbGateway())));
+        newUserViewModel.addObserver(newUserView);
 
+        UserDbGateway userDb = new UserDbGateway();
+        //userDb.fileReset();
+        userDb.updateUser(new FacilityUser("Jacob", "Password", UUID.randomUUID(), FacilityType.STORE));
+        userDb.updateUser(new AdminUser("Admin", "Password"));
         ProductDbGateway productDb = new ProductDbGateway();
         productDb.fileReset();
         productDb.updateProduct(new Product("Strawberries", 4001L, 2));
 
         FacilityDbGateway facilityDb = new FacilityDbGateway();
-        facilityDb.fileReset();
         Facility testFacility = new Facility("TestFacility", FacilityType.WAREHOUSE);
         testFacility.addProduct(4001L, 100);
+        // null pointer exception occurs on this line
         facilityDb.updateFacility(testFacility);
 
         OrderDbGateway orderDb = new OrderDbGateway();
