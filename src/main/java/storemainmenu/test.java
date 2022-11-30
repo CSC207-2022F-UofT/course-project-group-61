42b023a4be8c4302ccf812c31b366b79be1aec8e
Jacob DeSousa <jacobd2009@hotmail.com>
2022-11-30 17:07:54 -0500

    Update StoreMainMenu test to include new constructor with daily sales.

--- src/main/java/storemainmenu/test.java
+++ src/main/java/storemainmenu/test.java
@@ -1,12 +1,14 @@
 package storemainmenu;
 
+import dailysales.DailySalesViewModel;
 import order.OrderViewModel;
 
 public class test {
     public static void main(String[] args) {
         StoreMainMenuViewModel viewModel = new StoreMainMenuViewModel();
         OrderViewModel orderViewModel = new OrderViewModel();
-        StoreMainMenuView view = new StoreMainMenuView(new StoreMainMenuController(new StoreMainMenuPresenter(viewModel, orderViewModel)));
+        DailySalesViewModel dailySalesViewModel = new DailySalesViewModel();
+        StoreMainMenuView view = new StoreMainMenuView(new StoreMainMenuController(new StoreMainMenuPresenter(viewModel, orderViewModel, dailySalesViewModel)));
         viewModel.addObserver(view);
         viewModel.setVisible(true);
 
