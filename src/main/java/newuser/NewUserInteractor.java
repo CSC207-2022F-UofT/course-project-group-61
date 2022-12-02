2b31c99eaeed9066a35bbe82ecda3a24e7a54e4e
tadanokaida <kevinkhuang2002@gmail.com>
2022-12-02 15:41:19 -0500

    Add documentation

--- src/main/java/newuser/NewUserInteractor.java
+++ src/main/java/newuser/NewUserInteractor.java
@@ -32,16 +32,25 @@ public class NewUserInteractor implements NewUserInputBoundary{
 
         NewUserResponseModel response;
 
+        // Empty username - failure
         if (userDB.getUser(request.getUsername()) != null){
             response = new NewUserResponseModel(null, NewUserStatus.USERNAME_EXISTS);
             presenter.prepareFailView(response);
             return response;
         }
+        // Password too short - failure
         else if (request.getPassword().length() < 5){
             response = new NewUserResponseModel(null, NewUserStatus.PASSWORD_TOO_SHORT);
             presenter.prepareFailView(response);
             return response;
         }
+        // No registered facilities of the selected facility type - failure
+        else if (request.getFacilityID() == null){
+            response = new NewUserResponseModel(null, NewUserStatus.NO_FACILITIES);
+            presenter.prepareFailView(response);
+            return response;
+        }
+        // Success
         else {
             userDB.updateUser(newUser);
             response = new NewUserResponseModel(newUser, NewUserStatus.SUCCESS);
@@ -51,17 +60,22 @@ public class NewUserInteractor implements NewUserInputBoundary{
     }
 
     @Override
-    // returns an array list of two dictionaries mapping a UUID to a string (Facility Name)
+    // returns an array list of two dictionaries mapping a String (facility name) to the facility UUID.
     public ArrayList<HashMap<String, UUID>> getFacilityLists() {
+        // Hashmap mapping a store name to its UUID.
         HashMap<String, UUID> storeMap = new HashMap<>();
+        // Hashmap mapping a warehouse name to its UUID.
         HashMap<String, UUID> warehouseMap = new HashMap<>();
+        // Filling both hashmaps. The array will include all the facilities in the database.
         for (Map.Entry<UUID, Facility> facilityEntry: facilityDb.getAllFacilities().entrySet()){
             if (facilityEntry.getValue().getFacilityType() == FacilityType.STORE){
                 storeMap.put(facilityEntry.getValue().getName(), facilityEntry.getKey());
             } else if (facilityEntry.getValue().getFacilityType() == FacilityType.WAREHOUSE) {
                 warehouseMap.put(facilityEntry.getValue().getName(), facilityEntry.getKey());
             }
+            // leave open for any enhancements regarding new user/facility types.
         }
+        // Create and fill the arraylist.
         ArrayList<HashMap<String, UUID>> facilityList = new ArrayList<>();
         facilityList.add(storeMap);
         facilityList.add(warehouseMap);
--- src/main/java/newuser/NewUserView.java
+++ src/main/java/newuser/NewUserView.java
@@ -51,9 +51,13 @@ public class NewUserView extends JFrame implements Observer, ActionListener {
     }
 
     public void init(){
+        // Get the two maps from controller/interactor that map facility name (String) to facility UUID.
         this.storeMap = controller.getFacilityUUIDLists().get(0);
         this.warehouseMap = controller.getFacilityUUIDLists().get(1);
 
+        /* Create dropdown menus for stores and warehouses, where the user will select one facility.
+         - Only one dropdown menu is visible at a time (depending on what type of facility the user selects)
+         */
         this.storeList = new JComboBox<>();
         this.warehouseList = new JComboBox<>();
 
@@ -64,7 +68,8 @@ public class NewUserView extends JFrame implements Observer, ActionListener {
             warehouseList.addItem(warehouseName);
         }
 
-        this.uuid = storeMap.get(Objects.requireNonNull(storeList.getSelectedItem()).toString());
+        // Set the default UUID as the first store in the store dropdown menu (null if empty)
+        this.uuid = storeMap.get(storeList.getItemAt(0));
 
         JLabel header = new JLabel("Inventory Management System - New User");
         header.setFont(new Font("SansSerif", Font.PLAIN, 14));
@@ -74,21 +79,23 @@ public class NewUserView extends JFrame implements Observer, ActionListener {
         JLabel radioButtonSubtitle = new JLabel("Choose the facility type of the user's associated facility");
         radioButtonSubtitle.setFont(new Font("SansSerif", Font.PLAIN, 13));
 
+        // Add input fields for username and password of the new User
         usernameField = new JTextField("Username");
-        // want to add a "5 characters or longer" as a subtitle under password
         passwordField = new JTextField("Password (5 characters or longer)");
 
+        // Add radio buttons for the admin user to choose the new user's facility type
         this.storeButton = new JRadioButton("Store User");
         this.warehouseButton = new JRadioButton("Warehouse User");
 
+        // Set the default value of facility type to store (matching the default dropdown value defined above)
         storeButton.setSelected(true);
 
+        // Put both radio buttons in a group
         this.group = new ButtonGroup();
         group.add(storeButton);
         group.add(warehouseButton);
 
         this.registerButton = new JButton("Register");
-
 
         registerButton.addActionListener(this);
         storeButton.addActionListener(this);
@@ -128,19 +135,42 @@ public class NewUserView extends JFrame implements Observer, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Facility name from dropdown menu - used as a key in the Name to UUID hashmaps
         String facilityName;
+
+        // If store button is clicked, switch dropdown menu from warehouselist to storelist
         if (e.getSource() == storeButton) {
             warehouseList.setVisible(false);
             storeList.setVisible(true);
+
+            // Set UUID to the first (default) option in the store dropdown menu
+            this.uuid = storeMap.get(storeList.getItemAt(0));
+
+        // If warehouse button is clicked, switch dropdown menu from storelist to warehouselist
         } else if (e.getSource() == warehouseButton) {
             storeList.setVisible(false);
             warehouseList.setVisible(true);
+
+            // Set UUID to the first (default) option in the warehouse dropdown menu
+            this.uuid = storeMap.get(warehouseList.getItemAt(0));
+
+        /* If the store dropdown menu is clicked, update the UUID with the facility ID that corresponds to the
+           facility whose name was selected in the store dropdown menu.
+         */
         } else if (e.getSource() == storeList) {
              facilityName = (String) storeList.getSelectedItem();
              this.uuid = storeMap.get(facilityName);
+
+        /* If the warehouse dropdown menu is clicked, update the UUID with the facility ID that corresponds to the
+           facility whose name was selected in the warehouse dropdown menu.
+         */
         } else if (e.getSource() == warehouseList) {
             facilityName = (String) warehouseList.getSelectedItem();
             this.uuid = storeMap.get(facilityName);
+
+        /* If the register button is clicked, attempt to create a new user, with the facility type and UUID
+           corresponding to the options the user selected.
+         */
         } else if (e.getSource() == registerButton) {
             if (storeButton.isSelected()) {
                 controller.createStoreUser(usernameField.getText(), passwordField.getText(), uuid);
