71fb3572dbc5acc3e1ceaf95f0c654a32ff72292
tadanokaida <kevinkhuang2002@gmail.com>
2022-12-07 21:19:58 -0500

    Added facility name to table of information is store and warehouse main menus

--- src/main/java/warehousemainmenu/WarehouseMainMenuController.java
+++ src/main/java/warehousemainmenu/WarehouseMainMenuController.java
@@ -1,14 +1,22 @@
 package warehousemainmenu;
 
+import database.FacilityDbGateway;
+
 public class WarehouseMainMenuController {
 
     WarehouseMainMenuPresenter presenter;
-    public WarehouseMainMenuController(WarehouseMainMenuPresenter presenter) {
+    FacilityDbGateway facilityDB;
+    public WarehouseMainMenuController(WarehouseMainMenuPresenter presenter, FacilityDbGateway facilityDB) {
         this.presenter = presenter;
+        this.facilityDB = facilityDB;
     }
 
     /* Calls presenter.changeView based on what button sends input through the view. */
     public void chooseAction(ButtonOption option) {
         presenter.changeView(option);
+    }
+
+    public FacilityDbGateway getFacilityDB() {
+        return facilityDB;
     }
 }
